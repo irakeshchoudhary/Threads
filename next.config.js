aa54cd030685b0effbbb0d6f737f95e6c879b965
Rakesh Choudhary <zenerationmedia@gmail.com>
2024-07-26 21:05:18 +0530

    igonre ts errors

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  typescript: {
+    igonreBuildErrors: true,
+  },
   experimental: {
     serverActions: true,
     serverComponentsExternalPackages: ["mongoose"],
